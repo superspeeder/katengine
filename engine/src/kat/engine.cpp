cc26efcd75fc8ccb5030abbc5f8443043329c4ad
Andy Newton <andy@delusion.org>
2024-01-20 11:31:21 -0800

    gaming

--- engine/src/kat/engine.cpp
+++ engine/src/kat/engine.cpp
@@ -1,15 +1,23 @@
 #include "kat/engine.hpp"
 
-namespace kat
-{
-    Engine::Engine(const EngineConfig& engine_config)
-    {
 
+#include <unordered_set>
+
+namespace kat {
+    Engine::Engine(const EngineConfig &engine_config) {
+        create_window(engine_config.window_config);
+        create_instance();
+        create_surface();
+        select_physical_device();
+        select_queue_families();
+        create_device();
+        create_swapchain();
+        create_swapchain_image_views();
     }
 
-    Engine::~Engine()
-    {
-        for (auto iv : m_swapchain_image_views) m_device.destroy(iv);
+    Engine::~Engine() {
+        for (auto iv : m_swapchain_image_views)
+            m_device.destroy(iv);
 
         m_device.destroy(m_swapchain);
         m_device.destroy();
@@ -17,8 +25,7 @@ namespace kat
         m_instance.destroy();
     }
 
-    void Engine::create_window(const WindowConfig& config)
-    {
+    void Engine::create_window(const WindowConfig &config) {
         glfwInit();
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -26,100 +33,127 @@ namespace kat
         m_window = glfwCreateWindow(config.size.x, config.size.y, config.title.c_str(), nullptr, nullptr);
     }
 
-    void Engine::create_instance()
-    {
+    void Engine::create_instance() {
         vk::ApplicationInfo application_info{};
         application_info.setApiVersion(VK_API_VERSION_1_3);
 
-        uint32_t count;
-        const char** exts = glfwGetRequiredInstanceExtensions(&count);
+        uint32_t     count;
+        const char **exts = glfwGetRequiredInstanceExtensions(&count);
 
         m_instance = vk::createInstance(vk::InstanceCreateInfo({}, &application_info, 0, {}, count, exts));
     }
 
-    void Engine::select_physical_device()
-    {
+    void Engine::select_physical_device() {
         m_physical_device = m_instance.enumeratePhysicalDevices()[0];
     }
 
-    void Engine::create_surface()
-    {
+    void Engine::create_surface() {
         VkSurfaceKHR s;
         glfwCreateWindowSurface(m_instance, m_window, nullptr, &s);
         m_surface = s;
     }
 
-    void Engine::select_queue_families()
-    {
+    void Engine::select_queue_families() {
         std::optional<uint32_t> gt_family;
         std::optional<uint32_t> gtp_family;
 
-        auto families = m_physical_device.getQueueFamilyProperties();
-        uint32_t index = 0;
-        for (const auto& f : families)
-        {
+        auto     families = m_physical_device.getQueueFamilyProperties();
+        uint32_t index    = 0;
+        for (const auto &f : families) {
             bool g = false;
             bool p = false;
             bool t = false;
 
-            if (f.queueFlags & vk::QueueFlagBits::eGraphics)
-            {
-                g = true;
+            if (f.queueFlags & vk::QueueFlagBits::eGraphics) {
+                g                 = true;
                 m_graphics_family = index;
             }
 
-            if (f.queueFlags & vk::QueueFlagBits::eTransfer)
-            {
-                t = true;
+            if (f.queueFlags & vk::QueueFlagBits::eTransfer) {
+                t                 = true;
                 m_transfer_family = index;
             }
 
-            if (f.queueFlags & vk::QueueFlagBits::eCompute)
-            {
+            if (f.queueFlags & vk::QueueFlagBits::eCompute) {
                 m_compute_family = index;
             }
 
-            if (m_physical_device.getSurfaceSupportKHR(index, m_surface))
-            {
-                p = true;
+            if (m_physical_device.getSurfaceSupportKHR(index, m_surface)) {
+                p                = true;
                 m_present_family = index;
             }
 
-            if (g && t)
-            {
-                if (p)
-                {
+            if (g && t) {
+                if (p) {
                     gtp_family = index;
                 }
 
                 gt_family = index;
             }
 
-            if (m_graphics_family.has_value() && m_present_family.has_value() && m_transfer_family.has_value() && m_compute_family.has_value()) break;
+            if (m_graphics_family.has_value() && m_present_family.has_value() && m_transfer_family.has_value() &&
+                m_compute_family.has_value())
+                break;
         }
 
-
-        m_has_gt_family = gt_family.has_value();
+        m_has_gt_family   = gt_family.has_value();
         m_has_gtpc_family = gtp_family.has_value();
-        if (m_has_gtpc_family && *gtp_family == *m_compute_family) m_has_gtpc_family = true;
+        if (m_has_gtpc_family && *gtp_family == *m_compute_family)
+            m_has_gtpc_family = true;
 
-        if (m_has_gtp_family)
-        {
+        if (m_has_gtp_family) {
             m_graphics_family = gtp_family.value();
-            m_present_family = gtp_family.value();
+            m_present_family  = gtp_family.value();
             m_transfer_family = gtp_family.value();
         }
     }
 
-    void Engine::create_device()
-    {
+    void Engine::create_device() {
+        std::vector<const char *> exts = {
+            VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+        };
+
+        std::vector<vk::DeviceQueueCreateInfo> queue_create_infos;
+        const std::unordered_set<uint32_t>     unique_families = { m_graphics_family.value(), m_present_family.value(),
+                                                                   m_transfer_family.value(), m_compute_family.value() };
+        float                                  queue_priority  = 1.0f;
+
+        for (const auto &family : unique_families) {
+            queue_create_infos.emplace_back(vk::DeviceQueueCreateFlags{}, family, 1, &queue_priority);
+        }
+
+        vk::PhysicalDeviceFeatures2 features2{};
+        features2.features.geometryShader     = true;
+        features2.features.tessellationShader = true;
+        features2.features.largePoints        = true;
+        features2.features.wideLines          = true;
+        features2.features.fillModeNonSolid   = true;
+
+        auto create_info = vk::DeviceCreateInfo({}, queue_create_infos, {}, exts, {}, &features2);
+
+        m_device = m_physical_device.createDevice(create_info);
+
+        m_graphics_queue = m_device.getQueue(m_graphics_family.value(), 0);
+        m_transfer_queue = m_device.getQueue(m_transfer_family.value(), 0);
+        m_present_queue  = m_device.getQueue(m_present_family.value(), 0);
+        m_compute_queue  = m_device.getQueue(m_compute_family.value(), 0);
     }
 
-    void Engine::create_swapchain()
-    {
+    void Engine::create_swapchain() {
+        vk::SwapchainCreateInfoKHR create_info{};
+
+        auto caps = m_physical_device.getSurfaceCapabilitiesKHR(m_surface);
+        auto formats = m_physical_device.getSurfaceFormatsKHR(m_surface);
+        auto present_modes = m_physical_device.getSurfacePresentModesKHR(m_surface);
+
+        create_info.surface = m_surface;
+        create_info.minImageCount = caps.minImageCount + 1;
+        if (caps.maxImageCount > 0 && create_info.minImageCount > caps.maxImageCount) {
+            create_info.minImageCount = caps.maxImageCount;
+        }
+
+        create_info.imageFormat
     }
 
-    void Engine::create_swapchain_image_views()
-    {
-    }
-}
+    void Engine::create_swapchain_image_views() {}
+} // namespace kat
--- engine/src/kat/engine.hpp
+++ engine/src/kat/engine.hpp
@@ -1,40 +1,35 @@
 #pragma once
 
-#include <string>
 #include <glm/glm.hpp>
-
 #include <vulkan/vulkan.hpp>
 
 #include <GLFW/glfw3.h>
 
-namespace kat
-{
-    struct WindowConfig
-    {
+#include <string>
+
+namespace kat {
+    struct WindowConfig {
         std::string title = "Window";
-        glm::ivec2 size = {1200, 900};
+        glm::ivec2  size  = { 1200, 900 };
     };
 
-    struct SwapchainConfig
-    {
-        vk::Format format;
+    struct SwapchainConfig {
+        vk::Format        format;
         vk::ColorSpaceKHR color_space;
-        vk::Extent2D extent;
+        vk::Extent2D      extent;
     };
 
-    struct EngineConfig
-    {
+    struct EngineConfig {
         WindowConfig window_config{};
     };
 
-    class Engine
-    {
+    class Engine {
     public:
-        explicit Engine(const EngineConfig& engine_config = {});
+        explicit Engine(const EngineConfig &engine_config = {});
 
         ~Engine();
 
-        void create_window(const WindowConfig& config);
+        void create_window(const WindowConfig &config);
         void create_instance();
         void select_physical_device();
         void create_surface();
@@ -44,12 +39,12 @@ namespace kat
         void create_swapchain_image_views();
 
     private:
-        GLFWwindow* m_window;
+        GLFWwindow *m_window;
 
-        vk::Instance m_instance;
+        vk::Instance       m_instance;
         vk::PhysicalDevice m_physical_device;
-        vk::SurfaceKHR m_surface;
-        vk::Device m_device;
+        vk::SurfaceKHR     m_surface;
+        vk::Device         m_device;
 
         std::optional<uint32_t> m_transfer_family;
         std::optional<uint32_t> m_graphics_family;
@@ -60,14 +55,14 @@ namespace kat
         bool m_has_gtp_family;
         bool m_has_gtpc_family;
 
-        vk::Queue m_transfer_queue;
-        vk::Queue m_graphics_queue;
-        vk::Queue m_present_queue;
-        vk::Queue m_compute_queue;
+        vk::Queue        m_transfer_queue;
+        vk::Queue        m_graphics_queue;
+        vk::Queue        m_present_queue;
+        vk::Queue        m_compute_queue;
         vk::SwapchainKHR m_swapchain;
-        SwapchainConfig m_swapchain_config{};
+        SwapchainConfig  m_swapchain_config{};
 
-        std::vector<vk::Image> m_swapchain_images;
+        std::vector<vk::Image>     m_swapchain_images;
         std::vector<vk::ImageView> m_swapchain_image_views;
     };
-}
+} // namespace kat
